eb31c551e1c32a902a50e8d39404211f68e70d78
Lolita <lolita.danancher@wanadoo.fr>
2023-03-30 22:46:51 +0200

    comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 // Middleware to authenticate requests using JWT token
@@ -6,7 +7,7 @@ module.exports = (req, res, next) => {
     // Extract the token from the Authorization header
     const token = req.headers.authorization.split(" ")[1];
     // Verify the token with the secret key and get the decoded token object
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     // Extract the user ID from the decoded token and store it in the request object for later use
     const userId = decodedToken.userId;
     req.auth = {
